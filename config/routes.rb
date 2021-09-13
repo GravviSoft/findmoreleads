2da00062191aca2efd7f09fca81813d76cd9d65c
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-09-13 11:52:50 -0600

    Updated Controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'html_pages/help'
   get 'html_pages/about'
   get 'html_pages/pricing'
+  get 'html_pages/home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
