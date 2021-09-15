a04e12fd0b11bdd5c5974a14489428382eb1ca91
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-09-15 07:46:28 -0600

    Updated Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #Returns a full title on a per page basis
+  #Method with optional argument
+  def full_title(page_title = ' ')
+    base_title = "Find More Leads"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -4,27 +4,27 @@ class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_pages_home_url
     assert_response :success
-    assert_select "title", "Home | RRDP App"
+    assert_select "title", "Find More Leads"
   end
 
   test "should get help" do
     get html_pages_help_url
     assert_response :success
-    assert_select "title", "Help | RRDP App"
+    assert_select "title", "Help | Find More Leads"
 
   end
 
   test "should get about" do
     get html_pages_about_url
     assert_response :success
-    assert_select "title", "About | RRDP App"
+    assert_select "title", "About | Find More Leads"
 
   end
 
   test "should get pricing" do
     get html_pages_pricing_url
     assert_response :success
-    assert_select "title", "Pricing | RRDP App"
+    assert_select "title", "Pricing | Find More Leads"
 
   end
 
