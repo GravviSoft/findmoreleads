57e09aa32d8f4449016efdb2425bd70a9305ed2d
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-10-04 12:59:58 -0600

    All Quotes Datatable

--- db/migrate/20210930210641_add_columns_annual.rb
+++ db/migrate/20210930210641_add_columns_annual.rb
@@ -1,6 +1,6 @@
 class AddColumnsAnnual < ActiveRecord::Migration[6.1]
   def change
     add_column :qetquotes, :email, :string
-    add_column :qetquotes, :phone, :telephone_field
+    add_column :qetquotes, :phone, :integer
   end
 end
