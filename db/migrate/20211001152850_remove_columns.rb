7da9d9ef39ca1c2b27b3775cdcd46da750104238
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-10-04 13:07:09 -0600

    All Quotes Datatable

--- db/migrate/20211001152850_remove_columns.rb
+++ db/migrate/20211001152850_remove_columns.rb
@@ -1,15 +1,6 @@
 class RemoveColumns < ActiveRecord::Migration[6.1]
   def change
     remove_column :qetquotes, :zip
-    remove_column :qetquotes,  :singlefam
-    remove_column :qetquotes,  :condo
-    remove_column :qetquotes,  :coop
-    remove_column :qetquotes,  :other
-    remove_column :qetquotes,  :primaryres
-    remove_column :qetquotes,  :duplex
-    remove_column :qetquotes,  :threetofour
-    remove_column :qetquotes,  :mobilehome
-    remove_column :qetquotes,  :manufacturedhome
     remove_column :qetquotes,  :overthreeyears
     remove_column :qetquotes, :homeyear
     remove_column :qetquotes, :sqft
