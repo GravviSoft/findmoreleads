1ce4c068f0474288f8ce25dc2a7633ef08c9e7f9
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-10-04 18:40:20 -0600

    All Quotes Datatable

--- app/models/qetquote.rb
+++ app/models/qetquote.rb
@@ -2,7 +2,7 @@ class Qetquote < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :firstname, presence: true, length: {maximum: 60}
   validates :lastname, presence: true, length: {maximum: 60}
-  validates :phone, presence: true, length: {maximum: 10}
+  validates :phone, presence: true, length: {maximum: 11}
 
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: true}
