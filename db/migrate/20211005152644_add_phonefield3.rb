e85d24e8576f462c94d168de59aa1fc5491b4e07
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-10-05 09:35:49 -0600

    All Quotes Datatable

--- db/migrate/20211005152644_add_phonefield3.rb
+++ db/migrate/20211005152644_add_phonefield3.rb
@@ -1,5 +1,5 @@
 class AddPhonefield3 < ActiveRecord::Migration[6.1]
   def change
-    change_column :qetquotes, :phone, :integer, limit: 10
+    change_column :qetquotes, :phone, :integer, limit: 8
   end
 end
