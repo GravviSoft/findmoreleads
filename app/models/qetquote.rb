b6e9d1f1c216330c293a058819ec24d555b82e25
Beau Enslow <59984522+boboyoho@users.noreply.github.com>
2021-12-19 20:49:27 -0700

    all m

--- app/models/qetquote.rb
+++ app/models/qetquote.rb
@@ -7,5 +7,4 @@ class Qetquote < ApplicationRecord
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: true}
 
-
 end
